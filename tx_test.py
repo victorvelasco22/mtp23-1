ae3d93939a09a2633a1558bd7ad26f71fdaa6842
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-04-09 21:35:25 +0200

    Update tx_test.py

--- tx_test.py
+++ tx_test.py
@@ -89,41 +89,41 @@ if __name__ == "__main__":
  
             
          if not timeout:
-                if nrf.get_packages_lost() == 0:    
-                    # Check if an acknowledgement package is available.
-                    if nrf.data_ready():
-                        # Get payload.
-                        payload = nrf.get_payload()
+            if nrf.get_packages_lost() == 0:    
+                 # Check if an acknowledgement package is available.
+                 if nrf.data_ready():
+                     # Get payload.
+                     payload = nrf.get_payload()
         
-                        if len(payload) == 4:
-                            # If the payload is 4 bytes, we expect it to be an acknowledgement payload.
-                            (next_id, ) = struct.unpack('<I', payload)
+                     if len(payload) == 4:
+                         # If the payload is 4 bytes, we expect it to be an acknowledgement payload.
+                         (next_id, ) = struct.unpack('<I', payload)
 
-                        else:
-                            # Not 4 bytes long then we consider it an invalid payload.
-                            print("Invalid acknowledgement payload received.")
-                            next_id = -1
-                    else:
-                        print("No acknowledgement package received.")
-                        next_id = -1
+                     else:
+                         # Not 4 bytes long then we consider it an invalid payload.
+                         print("Invalid acknowledgement payload received.")
+                         next_id = -1
+                 else:
+                     print("No acknowledgement package received.")
+                     next_id = -1
     
-                else:
-                    # The package sent was lost.
-                    print("Package lost. No acknowledgement.")
-                    next_id = -1
-            else:
-                print("Timeout. No acknowledgement.")
-                next_id = -1
+             else:
+                 # The package sent was lost.
+                 print("Package lost. No acknowledgement.")
+                 next_id = -1
+         else:
+             print("Timeout. No acknowledgement.")
+             next_id = -1
                 
-            if timeout:
-                print(f'Error: timeout while waiting for acknowledgement package. next_id={next_id}')
-            else:  
-                if nrf.get_packages_lost() == 0:
-                    # The package we sent was successfully received by the server.
-                    print(f"Success: lost={nrf.get_packages_lost()}, retries={nrf.get_retries()}, next_id={next_id}")
-                else:
-                    print(f"Error: lost={nrf.get_packages_lost()}, retries={nrf.get_retries()}, next_id={next_id}")
-                    
+         if timeout:
+             print(f'Error: timeout while waiting for acknowledgement package. next_id={next_id}')
+         else:  
+             if nrf.get_packages_lost() == 0:
+                 # The package we sent was successfully received by the server.
+                 print(f"Success: lost={nrf.get_packages_lost()}, retries={nrf.get_retries()}, next_id={next_id}")
+             else:
+                 print(f"Error: lost={nrf.get_packages_lost()}, retries={nrf.get_retries()}, next_id={next_id}")
+                  
         print("sent OK!")
         fichero.close()    
         

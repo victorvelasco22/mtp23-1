eb9628da1af4551e5e8178083eeb98fd12b587f5
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-04-11 17:32:09 +0200

    Update rx.py

--- rx.py
+++ rx.py
@@ -3,7 +3,7 @@ from pyrf24 import RF24
 
 radio = RF24(22, 0)
 
-EOF = '/0xFF'
+#EOF = '/0xFF'
 
 if not radio.begin():
     raise OSError("nRF24L01 hardware isn't responding")
@@ -12,16 +12,14 @@ radio.channel = 90
 radio.listen = True
 
 eof = False
-payload = bytearray()
+payload = []
 
 try:
     while not eof:
         if radio.available():
             packet = radio.read()
-            if struct.unpack("<B",packet) == EOF:
-                eof = True
-            else:
-                payload.append(struct.unpack("<32s",packet))
+            payload.append(struct.unpack("<32s",packet))
+            print(payload)
     
     print("Transmission ok")
     text = str(payload,'utf-8')

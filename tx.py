a95cc8e7e411b0ca037e0669e05cb72008f8ad22
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-04-11 15:19:53 +0200

    Update tx.py

--- tx.py
+++ tx.py
@@ -15,31 +15,35 @@ radio.print_pretty_details()
 
 packets_sent = 0
 
-fichero = open("/home/hector/helloworld.txt", "r")
-text = fichero.read()
-text_bytes = bytes(text,'utf-8')
+fichero = open("/home/hector/helloworld.txt", "rb")
+#text = fichero.read()
+text_bytes = bytes(fichero,'utf-8')
+
+payload = []
+for i in range(0,len(text_bytes), 32):
+  payload[i] = text_bytes[i:i+31]
 
 #number of fragments with max payload (32 bytes)
-num_fragments = len(text_bytes) // 32
+#num_fragments = len(text_bytes) // 32
 
 #we create a payload list of 32 bytes
-payload = []
-j = 0
-for i in range(num_fragments):
-  payload.append([])
-  payload[i].append(text_bytes[j:j+31])
-  j += 32
+#payload = []
+#j = 0
+#for i in range(num_fragments):
+#  payload.append([])
+#  payload[i].append(text_bytes[j:j+31])
+#  j += 32
 #we fill the list with the remaining bytes
-remaining = len(text_bytes)%32
-payload.append([])
-payload[num_fragments].append(text_bytes[j:j+remaining]) 
+#remaining = len(text_bytes)%32
+#payload.append([])
+#payload[num_fragments].append(text_bytes[j:j+remaining]) 
 
 #put device in TX mode
 radio.listen = False
 
 try:
   for i in range(len(payload)):
-    message = struct.pack("<32s",bytes(payload[i]))
+    message = struct.pack("<32s",payload[i])
     ok = radio.write(message)
     packets_sent += 1
     print(f"Sending {packets_sent}...", ("ok" if ok else "failed"))
@@ -49,13 +53,13 @@ try:
   #packets_sent += 1
   #print(f"Sending {packets_sent}...", ("ok" if ok else "failed"))
     
-  message = struct.pack("<B",EOF)
-  ok = radio.write(message)
+  #message = struct.pack("<B",EOF)
+  #ok = radio.write(message)
   
-  if ok:
-    print("Transmission complete")
-  else:
-    print("Transmission failed")
+  #if ok:
+  #  print("Transmission complete")
+  #else:
+  #  print("Transmission failed")
   radio.power = False
 except KeyboardInterrupt:
   print("powering down radio and exiting.")

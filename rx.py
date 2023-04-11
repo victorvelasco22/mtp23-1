bc813d14ca7593fed50776d5f62f0823a95b4296
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-04-11 17:42:59 +0200

    Update rx.py

--- rx.py
+++ rx.py
@@ -1,6 +1,8 @@
 import struct
 from pyrf24 import RF24
 
+EOF = b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF' 
+
 radio = RF24(22, 0)
 
 #EOF = '/0xFF'
@@ -14,15 +16,20 @@ radio.listen = True
 
 eof = False
 payload = []
+received_packets = 0
 
 try:
     while not eof:
         if radio.available():
-            packet = radio.read()
-            payload.append(struct.unpack("<32s",packet))
-            print(payload)
-    
+            rx = radio.read()
+            packet = struct.unpack("<32s",rx)
+            if packet == EOF:
+                eof = True
+            else:
+                payload.append(packet)
+                received_packets += 1
     print("Transmission ok")
+    print(payload) 
     text = str(payload,'utf-8')
     print(text)
     radio.power = False

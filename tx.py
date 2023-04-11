832ba42856baf65132d5e7c3b8e43af7713e9fc0
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-04-11 12:23:28 +0200

    Create tx.py

---
+++ tx.py
@@ -0,0 +1,55 @@
+import struct
+import time
+from pyrf24 import RF24, RF24Network, RF24NetworkHeader
+
+radio = RF24(22, 0)
+
+EOF = '/0xFF'
+
+# initialize the nRF24L01 on the spi bus
+if not radio.begin():
+  raise OSError("nRF24L01 hardware isn't responding")
+
+radio.channel = 90
+radio.print_pretty_details()
+
+packets_sent = 0
+
+fichero = open("/home/hector/helloworld.txt", "r")
+text = fichero.read()
+text_bytes = bytearray(text,'utf-8')
+
+#number of fragments with max payload (32 bytes)
+num_fragments = len(text_bytes) // 32
+
+#we create a payload list of 32 bytes
+payload = []
+j = 0
+for i in range(num_fragments):
+  payload.append([])
+  payload[i].append(text_bytes[j:j+31])
+  j += 32
+#we fill the list with the remaining bytes
+payload.append([])
+payload[num_fragments].append(text_bytes[j:j+len(text_bytes)%32]) 
+
+#put device in TX mode
+radio.stopListening()
+
+try:
+  for i in range(len(payload)):
+    message = struct.pack("<32s",payload[i])
+    ok = radio.write(message)
+    packets_sent += 1
+    print(f"Sending {packets_sent}...", ("ok" if ok else "failed"))
+  
+  message = struct.pack("<B",EOF)
+  ok = radio.write(message)
+  if ok:
+    print("Transmission complete")
+  else:
+    print("Transmission failed")
+  
+except KeyboardInterrupt:
+  print("powering down radio and exiting.")
+  radio.power = False

f878aa291ce999372fff4af24115bf359fc22435
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-04-27 19:32:55 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def decodes(text):
 
 #read the utf-16-le file
 def open_txt():
-  with open("/home/rpi/mtp23/Quick Mode S23 Test File C.txt", "rb") as f:
+  with open("/home/rpi/mtp23/test_file_16_utf_le_1MB.txt", "rb") as f:
         text = f.read().decode("utf-16-le", errors="strict")
   return text
 

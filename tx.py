18248818cc4c53ea06acc4c12fe3f350cba4089f
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-04-27 19:25:38 +0200

    Update tx.py

--- tx.py
+++ tx.py
@@ -46,9 +46,10 @@ ok = False
 try:
   for i in range(len(payload)):
     message = struct.pack("<32s",payload[i])
-    ok = radio.write(message)
-    packets_sent += 1
-    print(f"Sending {packets_sent}...", ("ok" if ok else "failed"))
+    while not ok:
+      ok = radio.write(message)
+      packets_sent += 1
+      print(f"Sending {packets_sent}...", ("ok" if ok else "failed"))
     #print(message)
   message = struct.pack("<32s",EOF)
   ok = radio.write(message)

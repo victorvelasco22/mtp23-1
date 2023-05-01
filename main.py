d69fdbcf7755e3842c3b87044d88029709c3498f
victorvelasco22 <71555933+victorvelasco22@users.noreply.github.com>
2023-05-01 17:37:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO #importem la llibreria correpsonent
 from functions import *
 from pyrf24 import RF24
-import struct
 
 GPIO.setmode(GPIO.BCM) #establim com es fara referencia als pins de la RPi
 

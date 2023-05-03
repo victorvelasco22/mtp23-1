fa21d6d6dd475f3328c4a030cc481891a21f6a45
Nobitoso99 <130307477+Nobitoso99@users.noreply.github.com>
2023-05-03 15:27:21 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/bbt
-sudo python bbt.py
+cd home/rpi/mtp23
+sudo python tx_test.py
 cd /

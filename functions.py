d997f39f6662854f2bc4d75ae74ec2d2b86ed21e
josepbf <72404735+josepbf@users.noreply.github.com>
2023-05-09 23:37:09 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -85,13 +85,15 @@ def upload_to_usb(filename):
 def write(byte_txt):
   try:
       decompressed_bytes = decompress(byte_txt)
+      #with open("/media/rpi/USB/output.txt", mode="wb") as fichero:
+      with open("/home/rpi/textfile/output.txt", mode="ab") as fichero:
+        fichero.write(decompressed_bytes)
+      fichero.close()
     except:
       decompressed_bytes = None
       print("Error: Failed to decompress the batch.")
-  #with open("/media/rpi/USB/output.txt", mode="wb") as fichero:
-  with open("/home/rpi/textfile/output.txt", mode="ab") as fichero:
-      fichero.write(decompressed_bytes)
-  fichero.close()
+  
+  
 
 def tx(payload):
   total_packets_sent = 0

6ceb0372628e6f412446a6da553ea2dad3d24792
josepbf <72404735+josepbf@users.noreply.github.com>
2023-05-10 15:35:43 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -74,9 +74,7 @@ def rx():
 
 def upload_to_usb(filename):
   #path = str(filename,'utf-16-le').split("/")
-  name = str(filename,'utf-16-le')
-  print(name)
-  name = name.replace("TX","RX")
+  name = str(filename,'utf-16-le').replace("TX","RX")
   #shutil.copy("/home/rpi/textfile/output.txt", "/media/rpi/USB/output.txt")
   print(name + "a")
   shutil.copy("/home/rpi/textfile/output.txt", "/media/rpi/USB/"+name)

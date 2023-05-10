e974fd86bdcab05375d37c05c191e1f454d0b9e3
josepbf <72404735+josepbf@users.noreply.github.com>
2023-05-10 15:44:13 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -76,7 +76,7 @@ def upload_to_usb(filename):
   #path = str(filename,'utf-16-le').split("/")
   name = str(filename,'utf-16-le').replace("TX","RX")
   #shutil.copy("/home/rpi/textfile/output.txt", "/media/rpi/USB/output.txt")
-  name = name.replace('\0','')
+  name = name.replace('\0','-')
   print(name)
   shutil.copy("/home/rpi/textfile/output.txt", "/media/rpi/USB/"+name)
   print("Uploaded successfully")
